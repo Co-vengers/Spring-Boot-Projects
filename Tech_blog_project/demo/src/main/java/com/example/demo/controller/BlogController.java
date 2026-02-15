0e6c4126b58fd35f10afbeeb3f9d67eb3ac593ac
Co-vengers <vyomrohila@gmail.com>
2026-02-15 20:57:41 +0530

    Added Repository annotaion in repositories

--- Tech_blog_project/demo/src/main/java/com/example/demo/controller/BlogController.java
+++ Tech_blog_project/demo/src/main/java/com/example/demo/controller/BlogController.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+
 public class BlogController {
     private final BlogService blogService;
 
